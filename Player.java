52666f873fa0b8df12d592a4b47473a942ad7a97
MadHatterTenSix <beastwood2013@gmail.com>
2015-05-25 22:36:14 -0600

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,50 @@
+/**
+ *
+ * @author __MadHatter (alias used on https://www.reddit.com/r/dailyprogrammer)
+ */
+
+/* Player.java */
+
+import java.util.ArrayList;
+
+public class Player {
+
+  public static enum Type {
+    CPU,
+    HUMAN
+  }
+
+  private String name;
+  private Type type;
+  private ArrayList<Card> hand;
+
+  public Player(Type type, String name) {
+    hand      = new ArrayList<>();
+    this.type = type;
+    this.name = name;
+  }
+
+  public String getName()
+    { return name; }
+
+  public Type getType()
+    { return type; }
+
+  public ArrayList<Card> getHand()
+    { return hand; }
+
+  public void receiveCard(Card card) {
+    if (card != null) {
+      hand.add(card);
+    }
+  }
+
+  public void showHand() {
+    System.out.print("" + name + "'s cards: ");
+    for (int i = 0; i < hand.size(); i++) {
+      System.out.print("" + hand.get(i).toString() + "  ");
+    }
+    System.out.println("");
+  }
+
+}
