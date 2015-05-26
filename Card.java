de3ab2803aaaf37744aae1b42ceb71a8954c99a1
MadHatterTenSix <beastwood2013@gmail.com>
2015-05-25 22:35:15 -0600

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,56 @@
+/**
+ *
+ * @author __MadHatter (alias used on https://www.reddit.com/r/dailyprogrammer)
+ */
+
+/* Card.java */
+
+public class Card {
+
+  public final String SUITS[] = {
+    "UNKNOWN_SUIT",
+    "♥", "♣", "♦", "♠"
+  };
+
+  public final String WORD_VALUES[] = {
+    "UNKNOWN_WORD_VALUE", /* 0 value is meaningless */
+    "UNKNOWN_WORD_VALUE", /* 1 (ace) value moved to highest card */
+    "2", "3", "4", "5",
+    "6", "7", "8", "9", "10",
+    "J", "Q", "K", "A"
+  };
+
+  private int val;
+  private int suit;
+
+  public Card(int val, int suit) {
+
+    if (val <= 0 || val >= WORD_VALUES.length)
+      { this.val = 0; }
+    else
+      { this.val  = val; }
+
+    if (suit <= 0 || suit >= SUITS.length)
+      { this.suit = 0; }
+    else
+      { this.suit = suit; }
+
+  }
+
+  public int getSuit()
+    { return suit; }
+
+  public int getValue()
+    { return val; }
+
+  public String valueToString()
+    { return WORD_VALUES[val]; }
+
+  public String suitToString()
+    { return SUITS[suit]; }
+
+  @Override
+  public String toString()
+    { return ("" + suitToString() + valueToString()); }
+
+}
